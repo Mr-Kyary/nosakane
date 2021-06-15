b0b12d9cd4d4658aeb630de1f0144dcca1cb0d7b
Keita Karino <67830980+Mr-Kyary@users.noreply.github.com>
2021-06-15 17:28:42 +0900

    Googleカレンダーとの連携機能の完成。#7

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post '/callback' => 'line_bot#callback'
+
+  get "calendar/index", to:"calendar#index"
+  get "oauth2callback", to:"calendar#callback"
 end

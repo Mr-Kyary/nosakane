82cf8fc11072478c77965f465af482dc59c365ae
Keita Karino <67830980+Mr-Kyary@users.noreply.github.com>
2021-06-15 13:39:26 +0900

    LineBotコントローラーの作成。テスト処理の作成。

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :reports
   resources :report_types
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
+  post '/callback' => 'linebot#callback'
 end

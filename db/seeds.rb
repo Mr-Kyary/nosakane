4ee42ccea66779866424a06ba1a619cab1db5d0d
Keita Karino <67830980+Mr-Kyary@users.noreply.github.com>
2021-06-18 10:45:27 +0900

    投稿機能作成の作業中

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 管理者
+##################### 管理者 Admin
 Admin.create!(
   name: 'admin',
   email: 'admin@test.com',
   password: 'password'
 )
+##################### 管理者 Admin ここまで
+
+##################### ユーザー User
+Student.create!(
+  student_id: 202001,
+  name: 'てすと太郎',
+  line_account_id: 'lineaccountid1234'
+)
+##################### ユーザー User ここまで

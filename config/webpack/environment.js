e38c07b9e5f9e85c4f85b84999e47ea95f284ee5
MishimaKaiser <koushin.m.912@gmail.com>
2021-06-22 13:42:13 +0900

    ブートストラップの導入完了

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,24 @@
 const { environment } = require('@rails/webpacker')
-//jsを使えるように
-const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// ここから
+// jQueryとBootstapのJSを使えるように
+const webpack = require('C:\nosakane\app\javascript\packs')
 environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $:'jquery',
-        jQuery:'jquery',
-        Popper:'popper.js'
-    })
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
 )
+// ここまで
 
 module.exports = environment

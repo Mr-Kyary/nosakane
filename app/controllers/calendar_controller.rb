7c51d57a2189c557dfac173d4998de0bc6dcf058
Keita Karino <67830980+Mr-Kyary@users.noreply.github.com>
2021-06-25 09:43:09 +0900

    calendarコントローラーを編集

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -26,8 +26,7 @@ class CalendarController < ApplicationController
       credentials = authorizer.get_credentials(USER_ID)
       if credentials.nil?
         url = authorizer.get_authorization_url base_url: OOB_URI
-        puts "Open the following URL in the browser and enter the " \
-        "resulting code after authorization:\n" + url
+        redirect_to url
         code = gets
         credentials = authorizer.get_and_store_credentials_from_code(
           user_id: user_id, code: code, base_url: OOB_URI

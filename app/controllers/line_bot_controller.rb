e54304d574c2432d55bfb0a6711a1bf0fd2a40ef
Mr-Kyary <mr_kyary@icloud.com>
2021-06-30 23:17:46 +0900

    callback内処理変更。分岐処理に必要なカラムの策定。

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -20,74 +20,77 @@ class LineBotController < ApplicationController
 
     events = client.parse_events_from(body)
 
-    events.each { |event|
-      message = {
-        type: 'text',
-        text: "友達登録ありがとうございます！\n初めて利用する場合は学生番号を登録してください。"
-      }
-      client.reply_message(event['replyToken'], message)
+    state = 0
 
+    events.each { |event|
+      ###################################
+      # stateカラムは4桁の数字で構成
+      # ① ユーザ登録の有無 1:yes, 2:no
+      # ② 
+      # ③ 投稿フェーズの段階 
+      # ④
+      ###################################
       case event
+      when Line::Bot::Event::Follow
+        message = {
+          type: 'text',
+          text: "友達登録ありがとうございます！\n初めて利用する場合は学生番号を登録してください。"
+        }
+        client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          if event.message["text"].include?("1")
+        case 
+        when event.type == Line::Bot::Event::MessageType::Text
+          if event.message['text'].include?(">学生番号")
             message = {
               type: 'text',
-              text: "学生番号を登録します。"
+              text: "学生番号の登録を行います。\n学生番号を入力してください。"
             }
             client.reply_message(event['replyToken'], message)
-          elsif event.message["text"].include?("2")
+            # if文抜けるためのstateが必要
+            student_id = event.message['text']
             message = {
               type: 'text',
-              text: "就職活動を報告します。"
+              text: 'student name.'#が入力された学生番号で名前の確認
             }
-            client.reply_message(event['replyToken'], message)
-          elsif event.message["text"].include?("3")
-            message = {
-              type: 'text',
-              text: "nosakaneのホームページにアクセスします。"
-            }
-            client.reply_message(event['replyToken'], message)
-          else
-            message = {
-              type: 'text',
-              text: "リッチメニューから選んでください。"
-            }
-            client.reply_message(event['replyToken'], message)
+            client.reply_message(event['replyToken'], check_button(message))
+          elsif event.message['text'].include?(">活動報告")
+
+          elsif event.message['text'].include?(">活動報告の確認")
+
           end
         when Line::Bot::Event::MessageType::Image
           message = {
-            type: 'text',
-            text: "写真なんか送ってくんじゃねぇ！！"
+            type: 'image',
+            originalContentUrl: "https://f.easyuploader.app/20210630225256_6c393373.jpg",
+            previewImageUrl: "https://f.easyuploader.app/20210630225256_6c393373.jpg"
           }
-          client.reply_message(event['replyToken'], message)
+          client.reply_message(event['replyToken'], message)  
         end
       end
     }
   end
-end
 
-private
-def check_button(msg)
-{
-  "type": "template",
-  "altText": "this is a confirm template",
-  "template": {
-    "type": "confirm",
-    "actions": [
-        {
-          "type": "message",
-          "label": "はい",
-          "text": "はい"
-        },
-        {
-          "type": "message",
-          "label": "いいえ",
-          "text": "いいえ"
+  private
+  def check_button(msg)
+  {
+    "type": "template",
+    "altText": "this is a confirm template",
+    "template": {
+      "type": "confirm",
+      "actions": [
+          {
+            "type": "message",
+            "label": "はい",
+            "text": "はい"
+          },
+          {
+            "type": "message",
+            "label": "いいえ",
+            "text": "いいえ"
+          }
+          ],
+            "text": msg
         }
-        ],
-          "text": msg
-      }
-}
+  }
+  end
 end

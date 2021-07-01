c448c417a0dc9550e6defd6b0b8ebbb21817b99a
Mr-Kyary <mr_kyary@icloud.com>
2021-07-01 09:12:04 +0900

    LINEボット処理変更。

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -20,8 +20,6 @@ class LineBotController < ApplicationController
 
     events = client.parse_events_from(body)
 
-    state = 0
-
     events.each { |event|
       ###################################
       # stateカラムは4桁の数字で構成
@@ -30,32 +28,49 @@ class LineBotController < ApplicationController
       # ③ 投稿フェーズの段階 
       # ④
       ###################################
+      userId = event['source']['userId']
+      student = Student.where(line_account_id: userId).first
+
       case event
       when Line::Bot::Event::Follow
-        message = {
-          type: 'text',
-          text: "友達登録ありがとうございます！\n初めて利用する場合は学生番号を登録してください。"
-        }
-        client.reply_message(event['replyToken'], message)
+        unless student = Student.where(line_account_id: userId).first
+          student = Student.new
+          student.line_account_id = userId
+          student.state = 1
+          message = {
+            type: 'text',
+            text: "友達登録ありがとうございます！\n初めて利用する場合は下記URLより利用登録を行って下さい。\n\n#{URL_DOMAIN}/users/sign_up"
+          }
+          client.reply_message(event['replyToken'], message)
+          student.save
+        end
       when Line::Bot::Event::Message
-        case 
-        when event.type == Line::Bot::Event::MessageType::Text
-          if event.message['text'].include?(">学生番号")
-            message = {
-              type: 'text',
-              text: "学生番号の登録を行います。\n学生番号を入力してください。"
-            }
-            client.reply_message(event['replyToken'], message)
-            # if文抜けるためのstateが必要
-            student_id = event.message['text']
-            message = {
-              type: 'text',
-              text: 'student name.'#が入力された学生番号で名前の確認
-            }
-            client.reply_message(event['replyToken'], check_button(message))
-          elsif event.message['text'].include?(">活動報告")
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          if event.message['text'].include?(">学生番号") || student.state = 1
+            # message = {
+            #   type: 'text',
+            #   text: "学生番号の登録を行います。\n学生番号を入力してください。"
+            # }
+            # client.reply_message(event['replyToken'], message)
 
-          elsif event.message['text'].include?(">活動報告の確認")
+            if student = Student.where(student_id: event.message['text']).first
+              message = {
+                type: 'text',
+                text: s.name + "さんで登録をします。"
+                }
+              student.line_account_id = userId
+              student.state = 2
+              sstudent.save
+              client.reply_message(event['replyToken'], message)
+            else
+              message = {
+              type: 'text',
+              text: "該当の学生ナンバーがありません。\nもう一度学生ナンバーを入力してください"
+              }
+              client.reply_message(event['replyToken'], message)
+            end
+          elsif event.message['text'].include?(">活動報告") && 
 
           end
         when Line::Bot::Event::MessageType::Image
@@ -93,4 +108,6 @@ class LineBotController < ApplicationController
         }
   }
   end
+
+  def 
 end

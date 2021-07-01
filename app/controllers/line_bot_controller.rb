ac4a65b366b5c6edf9b4eb70495ab65350833b7f
Keita Karino <67830980+Mr-Kyary@users.noreply.github.com>
2021-07-02 08:45:07 +0900

    studentビュー内、stateの表示を追加。

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -22,71 +22,69 @@ class LineBotController < ApplicationController
 
     events.each { |event|
       ###################################
-      # stateカラムは4桁の数字で構成
-      # ① ユーザ登録の有無 1:yes, 2:no
-      # ② 
-      # ③ 投稿フェーズの段階 
-      # ④
+      # stateカラムは整数値
+      # 0: 初期値
+      # 1: 友達登録している（ブロックしていない）
+      # 2: 
+      # 3: 投稿フェーズの段階
+      # 4: 
       ###################################
       userId = event['source']['userId']
-      student = Student.where(line_account_id: userId).first
-
+      
+      unless student = Student.find_by(line_account_id: userId)
+        student = Student.new(line_account_id: userId)
+      end
+      
       case event
       when Line::Bot::Event::Follow
-        unless student = Student.where(line_account_id: userId).first
-          student = Student.new
-          student.line_account_id = userId
-          student.state = 1
-          message = {
-            type: 'text',
-            text: "友達登録ありがとうございます！\n初めて利用する場合は下記URLより利用登録を行って下さい。\n\n#{URL_DOMAIN}/users/sign_up"
-          }
-          client.reply_message(event['replyToken'], message)
-          student.save
-        end
+        message = {
+          type: 'text',
+          text: "友達登録ありがとうございます！\n初めて利用する場合は下記URLより利用登録を行って下さい。\n\n#{URL_DOMAIN}/users/sign_up"
+        }
+        client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          if event.message['text'].include?(">学生番号") || student.state = 1
-            # message = {
-            #   type: 'text',
-            #   text: "学生番号の登録を行います。\n学生番号を入力してください。"
-            # }
-            # client.reply_message(event['replyToken'], message)
+          if event.message['text'].include?(">学生番号")
+            student.state = 1
+          elsif event.message['text'].include?(">活動報告")
+            student.state = 2
+          end
 
-            if student = Student.where(student_id: event.message['text']).first
-              message = {
-                type: 'text',
-                text: s.name + "さんで登録をします。"
-                }
-              student.line_account_id = userId
-              student.state = 2
-              sstudent.save
-              client.reply_message(event['replyToken'], message)
-            else
-              message = {
+          case student.state
+          when nil
+            message = {
               type: 'text',
-              text: "該当の学生ナンバーがありません。\nもう一度学生ナンバーを入力してください"
-              }
-              client.reply_message(event['replyToken'], message)
-            end
-          elsif event.message['text'].include?(">活動報告") && 
-
+              text: student.name + "学生番号を登録します\n学生番号を入力してください。"
+            }
+          when 1
+            message = {
+              type: 'text',
+              text: student.name + "さんで登録をします。"
+            }
+            student.line_account_id = userId
+            student.state = 2
+            student.save
+            client.reply_message(event['replyToken'], message)
+          else
+            message = {
+            type: 'text',
+            text: "該当の学生ナンバーがありません。\nもう一度学生ナンバーを入力してください"
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+          elsif student.state == 2#student.state == 2 ←Studentで作成したインスタンスがない場合は？
+            create_report_on_line
           end
         when Line::Bot::Event::MessageType::Image
-          message = {
-            type: 'image',
-            originalContentUrl: "https://f.easyuploader.app/20210630225256_6c393373.jpg",
-            previewImageUrl: "https://f.easyuploader.app/20210630225256_6c393373.jpg"
-          }
-          client.reply_message(event['replyToken'], message)  
+          reply_sakane_picture
         end
       end
     }
   end
 
   private
-  def check_button(msg)
+  def check_button(msg)# LINEのYES/NOの確認メッセージを表示させる
   {
     "type": "template",
     "altText": "this is a confirm template",
@@ -109,5 +107,20 @@ class LineBotController < ApplicationController
   }
   end
 
-  def 
+  def create_report_on_line
+    message = {
+      type: 'text',
+      text: "投稿を完了しました。"
+      }
+      client.reply_message(event['replyToken'], message)
+  end
+  
+  def reply_sakane_picture
+    message = {
+      type: 'image',
+      originalContentUrl: "https://f.easyuploader.app/20210630225256_6c393373.jpg",
+      previewImageUrl:    "https://f.easyuploader.app/20210630225256_6c393373.jpg"
+    }
+    client.reply_message(event['replyToken'], message)
+  end
 end
